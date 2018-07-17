58c1fdf98b08544190b95709c92bfa9fe035f840
Basil Marti <basil.marti@unibas.ch>
2018-07-17 14:41:06 +0200

    Bugfixes für fix_doc_2gnd.pl

--- fix_doc_2gnd.pl
+++ fix_doc_2gnd.pl
@@ -1,37 +1,37 @@
 #!/exlibris/product/bin/perl -w
 ##########################################################################
-# fix_doc_2gnd Fuegt Unterfeld $$2gnd bei Sacherschliessungsfeldern hinzu
+# fix_doc_2gnd.pl
 ##########################################################################
 # speichern in $aleph_dev/aleph/exe
 # Skript muss ausfuehrbar gemacht werden (chmod 755)
 # einhaengen in tab_fix 
-##########################################################################
-#                                         erstellt  09/2015 bmt IDSBB
-
+#
 # Anforderungen: 
-# Bei allen Sacherschliessungsfeldern (6##), die GND-Verknuepfungen enthalten, wird automatisch $$2gnd hinzugefuegt. 
-
+# Bei allen Sacherschliessungsfeldern (6##), die GND-Verknuepfungen 
+# enthalten, wird automatisch $$2gnd hinzugefuegt. 
 ##########################################################################
+# xx.09.2015 Erstellt / IDSBB, bmt 
+# 17.07.2018 Bugfixes bei mehrfach vorkommenden Unterfeldern / IDSBB, bmt
+##########################################################################
+
 $ENV{NLS_LANG} = "AMERICAN_AMERICA.UTF8";
 
 use strict;
 
 my $line   = '';
 
-my $line1 = <STDIN>;                                              # weg damit (1. Zeile enthaelt keine Kategorien)
+my $line1 = <STDIN>;                                              # Weg damit (1. Zeile enthaelt keine Kategorien)
 while ($line = <STDIN>) {
    if ($line) {
       chomp $line; 	
-      if (substr($line, 0, 5) =~ /6(00|10|11|30|50|51).7/) {      #Fall 1: Sacherschliessungfeld mit Indikator2=7
-         $line =~ s/\$\$.\$\$/\$\$/g;                             #Loesche leere Unterfelder in der Feldmitte und am Feldenede
-         $line =~ s/\$\$.$//g;
-         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         #Pruefe ob $$1 (DE-588) * vorkommt
-            $line =~ s/\$\$2.+?(\$\$|$)/\$\$/g;                   #Loesche allfaellig vorhandenes Unterfeld $$2gnd 
-            $line =~ s/\$\$$//g;                                  
-            $line =~ s/$/\$\$2gnd/                                #Fuege Unterfeld $$2 gnd am Ende ein
+      if (substr($line, 0, 5) =~ /6(00|10|11|30|50|51).7/) {      # Fall 1: Sacherschliessungfeld mit Indikator 2=7
+         $line =~ s/\$\$.(?=(\$\$|$))//g;                         # Loesche leere Unterfelder in der Feldmitte und am Feldende
+         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         # Pruefe ob $$1 (DE-588) * vorkommt
+            $line =~ s/\$\$2.+?(?=(\$\$|$))//g;                   # Loesche allfaellig vorhandenes Unterfeld $$2gnd 
+            $line =~ s/$/\$\$2gnd/                                # Fuege Unterfeld $$2gnd am Ende ein
          }                                            
       }
-   print $line . "\n";                                            #Schreibe Feld wieder ins Katalogisat
+   print $line . "\n";                                            # Schreibe Feld wieder ins Katalogisat
    } else {
       last
    }
--- fix_doc_2gnd_han.pl
+++ fix_doc_2gnd_han.pl
@@ -1,50 +1,50 @@
 #!/exlibris/product/bin/perl -w
 ##########################################################################
-# fix_doc_2gnd Fuegt Unterfeld $$2gnd bei Sacherschliessungsfeldern hinzu
+# fix_doc_2gnd_han.pl 
 ##########################################################################
 # speichern in $aleph_dev/aleph/exe
 # Skript muss ausfuehrbar gemacht werden (chmod 755)
 # einhaengen in tab_fix 
-##########################################################################
-#                                         erstellt  09/2015 bmt IDSBB
-#                                         adaption fuer HAN 01/2016 bmt IDSBB
+#
 # Anforderungen: 
-# Bei allen Sacherschliessungsfeldern (6##), die GND-Verknuepfungen enthalten, wird automatisch $$2gnd hinzugefuegt. 
-# Falls keine GND-Nummer vorhanden ist wird $$2ressource hinzugefuegt
+# Bei allen Sacherschliessungsfeldern (6##), die GND-Verknuepfungen 
+# enthalten, wird automatisch $$2gnd hinzugefuegt. 
+# Falls keine GND-Nummer vorhanden ist, wird $$2resource hinzugefuegt
+# (ausser bei 650 und 651).
 ##########################################################################
+# xx.09.2015 Erstellt / IDSBB, bmt 
+# xx.01.2016 Adaption fuer HAN / IDSBB, bmt
+# 17.07.2018 Bugfixes bei mehrfach vorhandenen Unterfeldern / IDSBB, bmt
+##########################################################################
+
 $ENV{NLS_LANG} = "AMERICAN_AMERICA.UTF8";
 
 use strict;
 
 my $line   = '';
 
-my $line1 = <STDIN>;                                              # weg damit (1. Zeile enthaelt keine Kategorien)
+my $line1 = <STDIN>;                                              # Weg damit (1. Zeile enthaelt keine Kategorien)
 while ($line = <STDIN>) {                                           
    if ($line) {
       chomp $line; 	
-      if (substr($line, 0, 5) =~ /6(00|10|11|30).7/) {      #Fall 1: Sacherschliessungfeld mit Indikator2=7
-         $line =~ s/\$\$.\$\$/\$\$/g;                             #Loesche leere Unterfelder in der Feldmitte und am Feldenede
-         $line =~ s/\$\$.$//g;
-         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         #Pruefe ob $$1 (DE-588) * vorkommt
-            $line =~ s/\$\$2.+?\$\$/\$\$/g;                       #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/\$\$2.+?$//g;                              #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/$/\$\$2gnd/                                #Fuege Unterfeld $$2 gnd am Ende ein
+      if (substr($line, 0, 5) =~ /6(00|10|11|30).7/) {            # Fall 1: 600, 610, 611 oder 630 mit Indikator 2=7
+         $line =~ s/\$\$.(?=(\$\$|$))//g;                         # Loesche leere Unterfelder in der Feldmitte und am Feldenede
+         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         # Pruefe ob $$1 (DE-588) * vorkommt
+            $line =~ s/\$\$2.+?(?=(\$\$|$))//g;                   # Loesche allfaellig vorhandenes Unterfeld $$2
+            $line =~ s/$/\$\$2gnd/                                # Fuege Unterfeld $$2gnd am Ende ein
          } else {
-            $line =~ s/\$\$2.+?\$\$/\$\$/g;                       #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/\$\$2.+?$//g;                              #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/$/\$\$2resource/                           #Fuege Unterfeld $$2 ressource am Ende ein
+            $line =~ s/\$\$2.+?(?=(\$\$|$))//g;                   # Loesche allfaellig vorhandenes Unterfeld $$2
+            $line =~ s/$/\$\$2resource/                           # Fuege Unterfeld $$2resource am Ende ein
          }                                           
-      } elsif (substr($line, 0, 5) =~ /6(50|51).7/) {
-         $line =~ s/\$\$.\$\$/\$\$/g;                             #Loesche leere Unterfelder in der Feldmitte und am Feldenede
-         $line =~ s/\$\$.$//g;
-         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         #Pruefe ob $$1 (DE-588) * vorkommt
-            $line =~ s/\$\$2.+?\$\$/\$\$/g;                       #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/\$\$2.+?$//g;                              #Loesche allfaellig vorhandenes Unterfeld $$2
-            $line =~ s/$/\$\$2gnd/                                #Fuege Unterfeld $$2 gnd am Ende ein
+      } elsif (substr($line, 0, 5) =~ /6(50|51).7/) {             # Fall 2: 650 oder 651 mit Indikator 2=7
+         $line =~ s/\$\$.(?=(\$\$|$))//g;                         # Loesche leere Unterfelder in der Feldmitte und am Feldenede
+         if ( $line =~ /(\$\$1\(DE-588\).+?)(\$\$|$)/ ) {         # Pruefe ob $$1 (DE-588) * vorkommt
+            $line =~ s/\$\$2.+?(?=(\$\$|$))//g;                   # Loesche allfaellig vorhandenes Unterfeld $$2
+            $line =~ s/$/\$\$2gnd/                                # Fuege Unterfeld $$2gnd am Ende ein
          }
       }
 
-   print $line . "\n";                                            #Schreibe Feld wieder ins Katalogisat
+   print $line . "\n";                                            # Schreibe Feld wieder ins Katalogisat
    } else {
       last
    }
